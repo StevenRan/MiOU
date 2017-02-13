912de3ff8b71eec258515d6b1e7ba0bad0d30a83
Danny <lingbo_wx@hotmail.com>
2017-02-13 21:35:53 +0800

    Signed-off-by: Danny <lingbo_wx@hotmail.com>

---
+++ MiOU.Beans/Beans/BAvator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiOU.Entities.Beans
+{
+    public class BAvator:BObject
+    {
+        public bool Enabled { get; set; }
+        public BUser UpdatedBy { get; set; }
+        public BUser Owner { get; set; }
+        public BFile Image { get; set; }
+    }
+}
